df4b42c6410d2d474a4f389565f60bd8a1bb1840
Cristiano Cerqueira <114621346+CristianoCerqueira@users.noreply.github.com>
2022-10-14 19:50:14 -0300

    finalizei ex017

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,50 +1,22 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = window.document.getElementById(`txtano`)
-    var res = window.document.getElementById(`res`)
+function tabuada() {
+    let num = document.getElementById(`txtn`)
+    let tab = document.getElementById(`seltab`)
 
-    if (fano.value.length == 0 || fano.value > ano) {
-        window.alert (`[ERRO] Verifique os dados e tente novamente!`)
+    if (num.value.length == 0) {
+        window.alert(`Por favor, digite um número`)
     }
+
     else {
-        var fsex = window.document.getElementsByName(`radsex`)
-        var idade = ano - Number(fano.value)
-        var genero = ``
-        var img = document.createElement(`img`)
-        img.setAttribute(`id`, `foto`)
-
-        if (fsex[0].checked) {
-            genero = `Homem`
-            if (idade >= 0 && idade < 10) { //Criança
-                img.setAttribute(`src`, `bebehomem.png`)
-            }
-            else if (idade >= 10 && idade < 21) { //Jóvem
-                img.setAttribute(`src`, `jovemhomem.png`)
-            }
-            else if (idade < 50) { //Adulto
-                img.setAttribute(`src`, `adultohomem.png`)
-            }
-            else { //Idoso
-                img.setAttribute(`src`, `velhohomem.png`)
-            }
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ``
+        while (c <= 10) {
+            let item = document.createElement(`option`)
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-        else if (fsex[1].checked) {
-            genero = `Mulher`
-            if (idade >= 0 && idade < 10) { //Criança
-                img.setAttribute(`src`, `bebemulher.png`)
-            }
-            else if (idade >= 10 && idade < 21) { //Jóvem
-                img.setAttribute(`src`, `jovemmulher.png`)
-            }
-            else if (idade < 50) { //Adulto
-                img.setAttribute(`src`, `adultomulher.png`)
-            }
-            else { //Idoso
-                img.setAttribute(`src`, `velhomulher.png`)
-            }
-        }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-        res.appendChild(img)
     }
+
 }
